9403ff1ac5544ecc108cb34dc506540e6073f8f2
Dev Daksan <dev.91096@gmail.com>
2020-04-17 14:00:15 +0530

    Added Sass and Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+function Header() {
+  return (
+    <div className="header_cnt">
+      <h1>Clips</h1>
+    </div>
+  );
+}
+
+export default Header;
