2af604d115a8055d6cc7deacfbf011c6ef6447a0
Dev Daksan <dev.91096@gmail.com>
2020-04-18 20:58:34 +0530

    Added ParseFiles

--- src/App.js
+++ src/App.js
@@ -8,10 +8,57 @@ import Clips from './components/Clips';
 
 function App() {
   const [file, setFile] = useState('');
+  const [quotes, setQuotes] = useState([]);
 
   useEffect(() => {
-    console.log(file);
+    parseFile(file);
   }, [file]);
+
+  const parseFile = (file) => {
+    let quotesRaw = file.split('==========');
+    // Do Not Parse if the array is empty
+    if (quotesRaw.length < 2) return;
+
+    let quotes = quotesRaw
+      // eslint-disable-next-line
+      .map((quoteRaw) => {
+        let quoteDirty = quoteRaw.split('\n');
+        // Remove empty quotes from array
+        quoteRaw = quoteDirty.filter((q) => {
+          return q.length > 1;
+        });
+
+        let book = quoteRaw[0];
+        let quote = quoteRaw[2];
+        let location = '';
+        let time = '';
+
+        let locationsRaw = quoteRaw[1];
+
+        if (
+          (book !== undefined) &
+          (quote !== undefined) &
+          (locationsRaw !== undefined)
+        ) {
+          let locationsArray = locationsRaw.split(' ');
+          location = locationsArray[5].replace(/\r?\n|\r/g, '');
+          time = locationsArray
+            .slice(9, 15)
+            .join(' ')
+            .replace(/\r?\n|\r/g, '');
+
+          return {
+            book: book.replace(/\r?\n|\r/g, ''),
+            quote: quote.replace(/\r?\n|\r/g, ''),
+            location,
+            time,
+          };
+        }
+      })
+      .filter((q) => q);
+
+    setQuotes(quotes);
+  };
 
   return (
     <div className="App">
